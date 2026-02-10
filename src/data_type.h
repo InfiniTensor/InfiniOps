c590c2a2bb4e87dcc0365d8f63e4557dd539ebdd
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-02-10 17:48:17 +0800

    feat: add `DataType::FromString` for string-to-dtype conversion

--- src/data_type.h
+++ src/data_type.h
@@ -1,9 +1,11 @@
 #ifndef INFINI_OPS_DATA_TYPE_H_
 #define INFINI_OPS_DATA_TYPE_H_
 
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <string>
+#include <unordered_map>
 
 namespace infini::ops {
 
@@ -11,6 +13,8 @@ class DataType {
  public:
   constexpr DataType(int index, std::size_t element_size, const char* name)
       : index_{index}, element_size_{element_size}, name_{name} {}
+
+  static const DataType& FromString(const std::string& name);
 
   constexpr bool operator==(const DataType& other) const {
     return index_ == other.index_;
@@ -51,6 +55,18 @@ constexpr DataType kBFloat16{9, 2, "bfloat16"};
 constexpr DataType kFloat32{10, sizeof(float), "float32"};
 
 constexpr DataType kFloat64{11, sizeof(double), "float64"};
+
+inline const DataType& DataType::FromString(const std::string& name) {
+  static std::unordered_map<std::string, const DataType&> name_to_dtype{
+      {kInt8.name(), kInt8},       {kInt16.name(), kInt16},
+      {kInt32.name(), kInt32},     {kInt64.name(), kInt64},
+      {kUInt8.name(), kUInt8},     {kUInt16.name(), kUInt16},
+      {kUInt32.name(), kUInt32},   {kUInt64.name(), kUInt64},
+      {kFloat16.name(), kFloat16}, {kBFloat16.name(), kBFloat16},
+      {kFloat32.name(), kFloat32}, {kFloat64.name(), kFloat64}};
+
+  return name_to_dtype.at(name);
+}
 
 }  // namespace infini::ops
 
