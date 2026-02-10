acd8c88439856426a3cf64016a7a2b6e687b571c
Ziminli <coollizimin@gmail.com>
2026-02-10 07:25:36 +0000

    feat: extend Device enum and refactor GEMM support
    - Add additional entries to the Device enum class to support new hardware targets.
    - Adapt GEMM mcblas implementation to use MetaX backend and add the test example.
    - Extract common BLAS interfaces into a new blas.h abstraction for GEMM implementations to share.

--- src/nvidia/gemm/cublas.h
+++ src/nvidia/gemm/cublas.h
@@ -7,66 +7,37 @@
 #include "cublas_v2.h"
 // clang-format on
 
-#include "base/gemm.h"
+#include "cuda/gemm/blas.h"
 
 namespace infini::ops {
 
-template <>
-class Operator<Gemm, Device::Type::kNvidia> : public Gemm {
- public:
-  Operator(const Tensor a, const Tensor b, std::optional<float> alpha,
-           std::optional<float> beta, std::optional<int> trans_a,
-           std::optional<int> trans_b, Tensor c)
-      : Gemm{a.stride(0) == 1 ? a : b.T(),
-             a.stride(0) == 1 ? b : a.T(),
-             alpha,
-             beta,
-             trans_a,
-             trans_b,
-             a.stride(0) == 1 ? c : c.T()},
-        lda_{a_strides_[1]},
-        ldb_{b_strides_[1]},
-        ldc_{c_strides_[1]} {
-    // TODO: Check constraints.
+struct CudaBackend {
+  using blasHandle_t = cublasHandle_t;
+  using stream_t = cudaStream_t;
+
+  static void blasCreate(blasHandle_t* handle) { cublasCreate(handle); }
+
+  static void blasSetStream(blasHandle_t handle, stream_t stream) {
+    cublasSetStream(handle, stream);
   }
 
-  Operator(const Tensor a, const Tensor b, Tensor c)
-      : Operator{a, b, std::nullopt, std::nullopt, std::nullopt, std::nullopt,
-                 c} {}
-
-  void operator()(void* stream, const Tensor a, const Tensor b,
-                  std::optional<float> alpha, std::optional<float> beta,
-                  std::optional<int> trans_a, std::optional<int> trans_b,
-                  Tensor c) const override {
-    cublasHandle_t handle;
-    cublasCreate(&handle);
-
-    cublasSetStream(handle, static_cast<cudaStream_t>(stream));
-
-    const auto& alpha_value{alpha.value_or(alpha_)};
-    const auto& beta_value{beta.value_or(beta_)};
-    const auto& trans_a_value{alpha.value_or(trans_a_)};
-    const auto& trans_b_value{beta.value_or(trans_b_)};
-
-    // TODO: Add support for more data types.
-    assert(a_type_ == kFloat32 && b_type_ == kFloat32 && c_type_ == kFloat32 &&
-           "`operator()` not implemented for this data type");
-
-    cublasGemmEx(handle, trans_a_value ? CUBLAS_OP_T : CUBLAS_OP_N,
-                 trans_b_value ? CUBLAS_OP_T : CUBLAS_OP_N, m_, n_, k_,
-                 &alpha_value, b.data(), CUDA_R_32F, lda_, a.data(), CUDA_R_32F,
-                 ldb_, &beta_value, c.data(), CUDA_R_32F, ldc_,
+  static void blasGemmEx(blasHandle_t handle, bool transA, bool transB, int m,
+                         int n, int k, const float* alpha, const void* B,
+                         int ldb, const void* A, int lda, const float* beta,
+                         void* C, int ldc) {
+    cublasGemmEx(handle, transA ? CUBLAS_OP_T : CUBLAS_OP_N,
+                 transB ? CUBLAS_OP_T : CUBLAS_OP_N, m, n, k, alpha, B,
+                 CUDA_R_32F, ldb, A, CUDA_R_32F, lda, beta, C, CUDA_R_32F, ldc,
                  CUBLAS_COMPUTE_32F_FAST_TF32, CUBLAS_GEMM_DEFAULT);
-
-    cublasDestroy(handle);
   }
 
- private:
-  Tensor::Stride lda_{0};
+  static void blasDestroy(blasHandle_t handle) { cublasDestroy(handle); }
+};
 
-  Tensor::Stride ldb_{0};
-
-  Tensor::Stride ldc_{0};
+template <>
+class Operator<Gemm, Device::Type::kNvidia> : public Blas<CudaBackend> {
+ public:
+  using Blas<CudaBackend>::Blas;
 };
 
 }  // namespace infini::ops
