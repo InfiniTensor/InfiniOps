fc95cf5c21eddd08ac0ebbc98cf985166c5f7858
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-02-27 13:08:26 +0000

    fix: format `src/iluvatar/gemm/cublas.h`

--- src/iluvatar/gemm/cublas.h
+++ src/iluvatar/gemm/cublas.h
@@ -13,20 +13,27 @@ namespace infini::ops {
 
 struct IluvatarBackend {
   using blasHandle_t = cublasHandle_t;
+
   using stream_t = cudaStream_t;
 
   static constexpr auto BLAS_OP_N = CUBLAS_OP_N;
+
   static constexpr auto BLAS_OP_T = CUBLAS_OP_T;
+
   static constexpr auto R_32F = CUDA_R_32F;
+
   // Iluvatar uses `cudaDataType` for `computeType`, so we need to use
   // `CUDA_R_32F` instead of `CUBLAS_COMPUTE_32F_FAST_TF32`.
   static constexpr auto BLAS_COMPUTE_32F_FAST_TF32 = CUDA_R_32F;
+
   // Iluvatar uses `CUBLAS_GEMM_DEFAULT_TENSOR_OP` instead of
   // `CUBLAS_GEMM_DEFAULT`.
   static constexpr auto BLAS_GEMM_DEFAULT = CUBLAS_GEMM_DEFAULT_TENSOR_OP;
 
   static constexpr auto blasCreate = cublasCreate;
+
   static constexpr auto blasSetStream = cublasSetStream;
+
   static constexpr auto blasDestroy = cublasDestroy;
 
   static constexpr auto blasGemmStridedBatchedEx = [](auto&&... args) {
