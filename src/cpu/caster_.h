e7055a6649621d96afcc8b7b54a6e448b7227c00
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-03-24 06:59:28 +0000

    refactor: update CUDA kernels to use `Caster`

--- src/cpu/caster_.h
+++ src/cpu/caster_.h
@@ -7,43 +7,6 @@
 
 namespace infini::ops {
 
-namespace detail {
-
-template <typename T, typename = void>
-struct HasToFloat : std::false_type {};
-
-template <typename T>
-struct HasToFloat<T, std::void_t<decltype(std::declval<T>().ToFloat())>>
-    : std::true_type {};
-
-template <typename T, typename = void>
-struct HasFromFloat : std::false_type {};
-
-template <typename T>
-struct HasFromFloat<T,
-                    std::void_t<decltype(T::FromFloat(std::declval<float>()))>>
-    : std::true_type {};
-
-template <typename T>
-constexpr float ToFloatHelper(T&& x) {
-  if constexpr (HasToFloat<T>::value) {
-    return std::forward<T>(x).ToFloat();
-  } else {
-    return static_cast<float>(x);
-  }
-}
-
-template <typename PureDst>
-constexpr PureDst FromFloatHelper(float f) {
-  if constexpr (HasFromFloat<PureDst>::value) {
-    return PureDst::FromFloat(f);
-  } else {
-    return static_cast<PureDst>(f);
-  }
-}
-
-}  // namespace detail
-
 template <>
 struct Caster<Device::Type::kCpu> {
   template <typename Dst, typename Src>
@@ -64,8 +27,41 @@ struct Caster<Device::Type::kCpu> {
     if constexpr (!src_is_custom && !dst_is_custom) {
       return static_cast<PureDst>(std::forward<Src>(x));
     } else {
-      return detail::FromFloatHelper<PureDst>(
-          detail::ToFloatHelper(std::forward<Src>(x)));
+      return FromFloatHelper<PureDst>(ToFloatHelper(std::forward<Src>(x)));
+    }
+  }
+
+ private:
+  template <typename T, typename = void>
+  struct HasToFloat : std::false_type {};
+
+  template <typename T>
+  struct HasToFloat<T, std::void_t<decltype(std::declval<T>().ToFloat())>>
+      : std::true_type {};
+
+  template <typename T, typename = void>
+  struct HasFromFloat : std::false_type {};
+
+  template <typename T>
+  struct HasFromFloat<
+      T, std::void_t<decltype(T::FromFloat(std::declval<float>()))>>
+      : std::true_type {};
+
+  template <typename T>
+  static constexpr float ToFloatHelper(T&& x) {
+    if constexpr (HasToFloat<T>::value) {
+      return std::forward<T>(x).ToFloat();
+    } else {
+      return static_cast<float>(x);
+    }
+  }
+
+  template <typename PureDst>
+  static constexpr PureDst FromFloatHelper(float f) {
+    if constexpr (HasFromFloat<PureDst>::value) {
+      return PureDst::FromFloat(f);
+    } else {
+      return static_cast<PureDst>(f);
     }
   }
 };
