e18213a97c23cdf47f7fe9bdc60e233c4bc62ef0
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-04-02 07:12:10 +0000

    fix: introduce type conversion templates

--- src/cuda/caster.cuh
+++ src/cuda/caster.cuh
@@ -5,6 +5,21 @@
 
 namespace infini::ops {
 
+namespace detail {
+
+template <Device::Type kDev, typename T>
+struct ToFloat;
+
+template <Device::Type kDev, typename T>
+struct FromFloat;
+
+template <Device::Type kDev, typename Dst, typename Src>
+struct HardwareCast {
+  inline static constexpr bool kSupported = false;
+};
+
+}  // namespace detail
+
 template <Device::Type kDev>
 struct CudaCasterImpl {
   template <typename Dst, typename Src>
@@ -29,10 +44,8 @@ struct CudaCasterImpl {
   template <typename T>
   __host__ __device__ static constexpr float ToFloatHelper(T&& x) {
     using PureSrc = PureType<T>;
-    if constexpr (IsBFloat16<kDev, PureSrc>) {
-      return __bfloat162float(x);
-    } else if constexpr (IsFP16<kDev, PureSrc>) {
-      return __half2float(x);
+    if constexpr (IsBFloat16<kDev, PureSrc> || IsFP16<kDev, PureSrc>) {
+      return detail::ToFloat<kDev, PureSrc>{}(std::forward<T>(x));
     } else {
       return static_cast<float>(std::forward<T>(x));
     }
@@ -41,56 +54,28 @@ struct CudaCasterImpl {
   template <typename Dst>
   __host__ __device__ static constexpr Dst FromFloatHelper(float f) {
     using PureDst = PureType<Dst>;
-    if constexpr (IsBFloat16<kDev, PureDst>) {
-      return __float2bfloat16(f);
-    } else if constexpr (IsFP16<kDev, PureDst>) {
-      return __float2half(f);
+    if constexpr (IsBFloat16<kDev, PureDst> || IsFP16<kDev, PureDst>) {
+      return detail::FromFloat<kDev, PureDst>{}(f);
     } else {
       return static_cast<Dst>(f);
     }
   }
 
-  // Priority tags for overload resolution.
   struct PriorityLow {};
-
   struct PriorityHigh : PriorityLow {};
 
-  // Fallback: lowest priority. This always matches if nothing else does.
   template <typename Dst, typename Src>
   __host__ __device__ static constexpr Dst HardwareCast(Src&& x, PriorityLow) {
     return FromFloatHelper<Dst>(ToFloatHelper(std::forward<Src>(x)));
   }
 
-// Usage: `DEFINE_DIRECT_CAST(INTRINSIC, CONDITION)`.
-#define DEFINE_DIRECT_CAST(INTRINSIC, ...)                            \
-  template <typename Dst, typename Src>                               \
-  __host__ __device__ static auto HardwareCast(Src x, PriorityHigh)   \
-      -> std::enable_if_t<(__VA_ARGS__),                              \
-                          decltype(INTRINSIC(std::declval<Src>()))> { \
-    return INTRINSIC(x);                                              \
+  template <typename Dst, typename Src>
+  __host__ __device__ static auto HardwareCast(Src x, PriorityHigh)
+      -> std::enable_if_t<
+          detail::HardwareCast<kDev, PureType<Dst>, PureType<Src>>::kSupported,
+          Dst> {
+    return detail::HardwareCast<kDev, PureType<Dst>, PureType<Src>>{}(x);
   }
-
-  DEFINE_DIRECT_CAST(
-      __bfloat162int_rn,
-      std::is_same_v<PureType<Dst>, int>&& IsBFloat16<kDev, PureType<Src>>)
-  DEFINE_DIRECT_CAST(
-      __bfloat162short_rn,
-      std::is_same_v<PureType<Dst>, short>&& IsBFloat16<kDev, PureType<Src>>)
-  DEFINE_DIRECT_CAST(
-      __int2bfloat16_rn,
-      IsBFloat16<kDev, PureType<Dst>>&& std::is_same_v<PureType<Src>, int>)
-  DEFINE_DIRECT_CAST(
-      __int2half_rn,
-      IsFP16<kDev, PureType<Dst>>&& std::is_same_v<PureType<Src>, int>)
-  DEFINE_DIRECT_CAST(
-      __double2bfloat16,
-      IsBFloat16<kDev, PureType<Dst>>&& std::is_same_v<PureType<Src>, double>)
-  DEFINE_DIRECT_CAST(
-      __double2half,
-      IsFP16<kDev, PureType<Dst>>&& std::is_same_v<PureType<Src>, double>)
-  DEFINE_DIRECT_CAST(
-      __half, IsFP16<kDev, PureType<Dst>>&& IsBFloat16<kDev, PureType<Src>>)
-#undef DEFINE_DIRECT_CAST
 };
 
 }  // namespace infini::ops
