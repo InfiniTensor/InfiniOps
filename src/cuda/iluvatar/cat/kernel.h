dd644d79036e9849dca0ea9c871d6ef28b64451c
gongchensu <zhuyue_134@qq.com>
2026-05-06 07:55:44 +0000

    feat(cuda): add cat support for CUDA-like backends
    Add a generic CUDA cat kernel plus NVIDIA/Iluvatar/MetaX/Moore specializations,
    and cover empty-output regression in tests.

---
+++ src/cuda/iluvatar/cat/kernel.h
@@ -0,0 +1,21 @@
+#ifndef INFINI_OPS_ILUVATAR_CAT_KERNEL_H_
+#define INFINI_OPS_ILUVATAR_CAT_KERNEL_H_
+
+#include <utility>
+
+#include "cuda/cat/kernel.h"
+#include "cuda/iluvatar/caster.cuh"
+#include "cuda/iluvatar/runtime_.h"
+
+namespace infini::ops {
+
+template <>
+class Operator<Cat, Device::Type::kIluvatar>
+    : public CudaCat<Runtime<Device::Type::kIluvatar>> {
+ public:
+  using CudaCat<Runtime<Device::Type::kIluvatar>>::CudaCat;
+};
+
+}  // namespace infini::ops
+
+#endif
