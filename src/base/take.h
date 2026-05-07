1a6a03fb39ef0ec1cdfc07248c34258006ef32e5
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:27 +0800

    chore: separate base class members

--- src/base/take.h
+++ src/base/take.h
@@ -24,14 +24,23 @@ class Take : public Operator<Take> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape index_shape_;
+
   Tensor::Strides index_strides_;
+
   DataType index_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
