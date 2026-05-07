3e48dda6163d9ee8ae0a2f110bced6cfbadf2a22
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:53:30 +0800

    feat: add `softplus` base

---
+++ src/base/softplus.h
@@ -0,0 +1,35 @@
+#ifndef INFINI_OPS_BASE_SOFTPLUS_H_
+#define INFINI_OPS_BASE_SOFTPLUS_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class Softplus : public Operator<Softplus> {
+ public:
+  Softplus(const Tensor self, const double beta, const double threshold,
+           Tensor out)
+      : self_shape_{self.shape()},
+        self_strides_{self.strides()},
+        self_type_{self.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor self, const double beta,
+                          const double threshold, Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape self_shape_;
+  Tensor::Strides self_strides_;
+  DataType self_type_;
+  Tensor::Shape out_shape_;
+  Tensor::Strides out_strides_;
+  DataType out_type_;
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
