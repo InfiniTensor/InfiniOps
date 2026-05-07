104d7c21bb4d71919c1495b6b4b6bc51897cc149
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:03 +0800

    chore: separate base class members

--- src/base/less_scalar.h
+++ src/base/less_scalar.h
@@ -21,11 +21,17 @@ class LessScalar : public Operator<LessScalar> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
