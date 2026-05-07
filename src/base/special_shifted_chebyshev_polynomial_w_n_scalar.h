1b20c5fe0ddd6480fbabf4184166386eaf51bad2
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:53:35 +0800

    feat: add `special_shifted_chebyshev_polynomial_w_n_scalar` base

---
+++ src/base/special_shifted_chebyshev_polynomial_w_n_scalar.h
@@ -0,0 +1,35 @@
+#ifndef INFINI_OPS_BASE_SPECIAL_SHIFTED_CHEBYSHEV_POLYNOMIAL_W_N_SCALAR_H_
+#define INFINI_OPS_BASE_SPECIAL_SHIFTED_CHEBYSHEV_POLYNOMIAL_W_N_SCALAR_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class SpecialShiftedChebyshevPolynomialWNScalar
+    : public Operator<SpecialShiftedChebyshevPolynomialWNScalar> {
+ public:
+  SpecialShiftedChebyshevPolynomialWNScalar(const Tensor x, const double n,
+                                            Tensor out)
+      : x_shape_{x.shape()},
+        x_strides_{x.strides()},
+        x_type_{x.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor x, const double n, Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape x_shape_;
+  Tensor::Strides x_strides_;
+  DataType x_type_;
+  Tensor::Shape out_shape_;
+  Tensor::Strides out_strides_;
+  DataType out_type_;
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
