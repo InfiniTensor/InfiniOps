3adead1607f3aa9e89a9d8716400ef7df3ebf5f3
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:36:53 +0800

    chore: separate base class members

--- src/base/cudnn_convolution.h
+++ src/base/cudnn_convolution.h
@@ -34,14 +34,23 @@ class CudnnConvolution : public Operator<CudnnConvolution> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape weight_shape_;
+
   Tensor::Strides weight_strides_;
+
   DataType weight_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
