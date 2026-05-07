1b30ebe32435876a54f623123ff57403009797e7
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:36:46 +0800

    chore: separate base class members

--- src/base/adaptive_max_pool3d.h
+++ src/base/adaptive_max_pool3d.h
@@ -26,14 +26,23 @@ class AdaptiveMaxPool3d : public Operator<AdaptiveMaxPool3d> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   Tensor::Shape indices_shape_;
+
   Tensor::Strides indices_strides_;
+
   DataType indices_type_;
+
   int device_index_{0};
 };
 
