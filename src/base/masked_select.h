847edbc60e4d69662e71f715c7267c7f4d0598b0
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:10 +0800

    chore: separate base class members

--- src/base/masked_select.h
+++ src/base/masked_select.h
@@ -24,14 +24,23 @@ class MaskedSelect : public Operator<MaskedSelect> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape mask_shape_;
+
   Tensor::Strides mask_strides_;
+
   DataType mask_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
