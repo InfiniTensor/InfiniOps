8fba1203cc2012ee3051528dfe4064b8d0a9f785
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:36:48 +0800

    chore: separate base class members

--- src/base/arctanh.h
+++ src/base/arctanh.h
@@ -20,11 +20,17 @@ class Arctanh : public Operator<Arctanh> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
