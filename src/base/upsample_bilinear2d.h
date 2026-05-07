ffcc2296eadb7f409e2c04e1d8dfeabe05569532
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:29 +0800

    chore: separate base class members

--- src/base/upsample_bilinear2d.h
+++ src/base/upsample_bilinear2d.h
@@ -23,11 +23,17 @@ class UpsampleBilinear2d : public Operator<UpsampleBilinear2d> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
