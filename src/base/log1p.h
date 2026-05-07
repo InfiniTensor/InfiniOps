42855c7476b9b5efe8746fb67c1c16502e3e57f6
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:08 +0800

    chore: separate base class members

--- src/base/log1p.h
+++ src/base/log1p.h
@@ -20,11 +20,17 @@ class Log1p : public Operator<Log1p> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
