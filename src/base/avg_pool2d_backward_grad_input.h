c9de04605776196388c0b295e50bf98c98333f50
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:53:39 +0800

    feat: add `avg_pool2d_backward_grad_input` base

---
+++ src/base/avg_pool2d_backward_grad_input.h
@@ -0,0 +1,49 @@
+#ifndef INFINI_OPS_BASE_AVG_POOL2D_BACKWARD_GRAD_INPUT_H_
+#define INFINI_OPS_BASE_AVG_POOL2D_BACKWARD_GRAD_INPUT_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class AvgPool2dBackwardGradInput : public Operator<AvgPool2dBackwardGradInput> {
+ public:
+  AvgPool2dBackwardGradInput(const Tensor grad_output, const Tensor self,
+                             const std::vector<int64_t> kernel_size,
+                             const std::vector<int64_t> stride,
+                             const std::vector<int64_t> padding,
+                             const bool ceil_mode, const bool count_include_pad,
+                             Tensor grad_input)
+      : grad_output_shape_{grad_output.shape()},
+        grad_output_strides_{grad_output.strides()},
+        grad_output_type_{grad_output.dtype()},
+        self_shape_{self.shape()},
+        self_strides_{self.strides()},
+        self_type_{self.dtype()},
+        grad_input_shape_{grad_input.shape()},
+        grad_input_strides_{grad_input.strides()},
+        grad_input_type_{grad_input.dtype()},
+        device_index_{grad_input.device().index()} {}
+
+  virtual void operator()(const Tensor grad_output, const Tensor self,
+                          const std::vector<int64_t> kernel_size,
+                          const std::vector<int64_t> stride,
+                          const std::vector<int64_t> padding,
+                          const bool ceil_mode, const bool count_include_pad,
+                          Tensor grad_input) const = 0;
+
+ protected:
+  Tensor::Shape grad_output_shape_;
+  Tensor::Strides grad_output_strides_;
+  DataType grad_output_type_;
+  Tensor::Shape self_shape_;
+  Tensor::Strides self_strides_;
+  DataType self_type_;
+  Tensor::Shape grad_input_shape_;
+  Tensor::Strides grad_input_strides_;
+  DataType grad_input_type_;
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
