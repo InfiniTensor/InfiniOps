86f407fe50d43a2244b3ebabc1c8003bc2f0c958
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:36:57 +0800

    chore: separate base class members

--- src/base/float_power_tensor_tensor.h
+++ src/base/float_power_tensor_tensor.h
@@ -24,14 +24,23 @@ class FloatPowerTensorTensor : public Operator<FloatPowerTensorTensor> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape exponent_shape_;
+
   Tensor::Strides exponent_strides_;
+
   DataType exponent_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
