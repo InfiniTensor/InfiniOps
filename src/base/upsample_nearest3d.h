9799d896912deef68fa33a443942feb89e287991
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:29 +0800

    chore: separate base class members

--- src/base/upsample_nearest3d.h
+++ src/base/upsample_nearest3d.h
@@ -23,11 +23,17 @@ class UpsampleNearest3d : public Operator<UpsampleNearest3d> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
