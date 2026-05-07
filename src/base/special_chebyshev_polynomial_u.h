e9f7f4ec0a93e8d772fc496127ae034dede59d10
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:20 +0800

    chore: separate base class members

--- src/base/special_chebyshev_polynomial_u.h
+++ src/base/special_chebyshev_polynomial_u.h
@@ -24,14 +24,23 @@ class SpecialChebyshevPolynomialU
 
  protected:
   Tensor::Shape x_shape_;
+
   Tensor::Strides x_strides_;
+
   DataType x_type_;
+
   Tensor::Shape n_shape_;
+
   Tensor::Strides n_strides_;
+
   DataType n_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
