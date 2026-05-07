7431541ef314a5b08cc948577245c5fe4221285b
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:06 +0800

    chore: separate base class members

--- src/base/linalg_pinv_rcond_tensor.h
+++ src/base/linalg_pinv_rcond_tensor.h
@@ -24,14 +24,23 @@ class LinalgPinvRcondTensor : public Operator<LinalgPinvRcondTensor> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape rcond_shape_;
+
   Tensor::Strides rcond_strides_;
+
   DataType rcond_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
