faf0974c705e43f247b94b77cca397890bd379e3
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:53:37 +0800

    feat: add `take_along_dim` base

---
+++ src/base/take_along_dim.h
@@ -0,0 +1,40 @@
+#ifndef INFINI_OPS_BASE_TAKE_ALONG_DIM_H_
+#define INFINI_OPS_BASE_TAKE_ALONG_DIM_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class TakeAlongDim : public Operator<TakeAlongDim> {
+ public:
+  TakeAlongDim(const Tensor self, const Tensor indices, Tensor out)
+      : self_shape_{self.shape()},
+        self_strides_{self.strides()},
+        self_type_{self.dtype()},
+        indices_shape_{indices.shape()},
+        indices_strides_{indices.strides()},
+        indices_type_{indices.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor self, const Tensor indices,
+                          Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape self_shape_;
+  Tensor::Strides self_strides_;
+  DataType self_type_;
+  Tensor::Shape indices_shape_;
+  Tensor::Strides indices_strides_;
+  DataType indices_type_;
+  Tensor::Shape out_shape_;
+  Tensor::Strides out_strides_;
+  DataType out_type_;
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
