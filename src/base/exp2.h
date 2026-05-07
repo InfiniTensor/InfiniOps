21bd8caed89a43ab39719b826e7cd4427f47c795
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:36:55 +0800

    chore: separate base class members

--- src/base/exp2.h
+++ src/base/exp2.h
@@ -20,11 +20,17 @@ class Exp2 : public Operator<Exp2> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
