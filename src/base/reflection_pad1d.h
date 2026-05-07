02897c61b2f7d02ae639b8d74bf694f506867dcc
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:15 +0800

    chore: separate base class members

--- src/base/reflection_pad1d.h
+++ src/base/reflection_pad1d.h
@@ -22,11 +22,17 @@ class ReflectionPad1d : public Operator<ReflectionPad1d> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
