f4b88941a312d38658fc4e10ae8139875782b48e
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:05 +0800

    chore: separate base class members

--- src/base/linalg_matrix_power.h
+++ src/base/linalg_matrix_power.h
@@ -21,11 +21,17 @@ class LinalgMatrixPower : public Operator<LinalgMatrixPower> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
