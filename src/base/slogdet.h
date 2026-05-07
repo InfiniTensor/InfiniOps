1c04f46a926650605f14831be7173b9f62bcfc9f
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:53:29 +0800

    feat: add `slogdet` base

---
+++ src/base/slogdet.h
@@ -0,0 +1,40 @@
+#ifndef INFINI_OPS_BASE_SLOGDET_H_
+#define INFINI_OPS_BASE_SLOGDET_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class Slogdet : public Operator<Slogdet> {
+ public:
+  Slogdet(const Tensor self, Tensor sign, Tensor logabsdet)
+      : self_shape_{self.shape()},
+        self_strides_{self.strides()},
+        self_type_{self.dtype()},
+        sign_shape_{sign.shape()},
+        sign_strides_{sign.strides()},
+        sign_type_{sign.dtype()},
+        logabsdet_shape_{logabsdet.shape()},
+        logabsdet_strides_{logabsdet.strides()},
+        logabsdet_type_{logabsdet.dtype()},
+        device_index_{sign.device().index()} {}
+
+  virtual void operator()(const Tensor self, Tensor sign,
+                          Tensor logabsdet) const = 0;
+
+ protected:
+  Tensor::Shape self_shape_;
+  Tensor::Strides self_strides_;
+  DataType self_type_;
+  Tensor::Shape sign_shape_;
+  Tensor::Strides sign_strides_;
+  DataType sign_type_;
+  Tensor::Shape logabsdet_shape_;
+  Tensor::Strides logabsdet_strides_;
+  DataType logabsdet_type_;
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
