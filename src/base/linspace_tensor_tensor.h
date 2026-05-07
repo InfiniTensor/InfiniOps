1164040c20137034cc9364dc63e2402a388b7062
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:53:22 +0800

    feat: add `linspace_tensor_tensor` base

---
+++ src/base/linspace_tensor_tensor.h
@@ -0,0 +1,41 @@
+#ifndef INFINI_OPS_BASE_LINSPACE_TENSOR_TENSOR_H_
+#define INFINI_OPS_BASE_LINSPACE_TENSOR_TENSOR_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class LinspaceTensorTensor : public Operator<LinspaceTensorTensor> {
+ public:
+  LinspaceTensorTensor(const Tensor start, const Tensor end,
+                       const int64_t steps, Tensor out)
+      : start_shape_{start.shape()},
+        start_strides_{start.strides()},
+        start_type_{start.dtype()},
+        end_shape_{end.shape()},
+        end_strides_{end.strides()},
+        end_type_{end.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor start, const Tensor end,
+                          const int64_t steps, Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape start_shape_;
+  Tensor::Strides start_strides_;
+  DataType start_type_;
+  Tensor::Shape end_shape_;
+  Tensor::Strides end_strides_;
+  DataType end_type_;
+  Tensor::Shape out_shape_;
+  Tensor::Strides out_strides_;
+  DataType out_type_;
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
