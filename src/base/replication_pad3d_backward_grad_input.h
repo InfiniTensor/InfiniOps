b73d0129f708e6ca77241ef54e58bb34466d6ee4
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:35 +0800

    chore: separate base class members

--- src/base/replication_pad3d_backward_grad_input.h
+++ src/base/replication_pad3d_backward_grad_input.h
@@ -28,14 +28,23 @@ class ReplicationPad3dBackwardGradInput
 
  protected:
   Tensor::Shape grad_output_shape_;
+
   Tensor::Strides grad_output_strides_;
+
   DataType grad_output_type_;
+
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape grad_input_shape_;
+
   Tensor::Strides grad_input_strides_;
+
   DataType grad_input_type_;
+
   int device_index_{0};
 };
 
