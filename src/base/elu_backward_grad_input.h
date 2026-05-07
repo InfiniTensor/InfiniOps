4169a9becb04cbdc431e87f2ca7d176d2172643a
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:30 +0800

    chore: separate base class members

--- src/base/elu_backward_grad_input.h
+++ src/base/elu_backward_grad_input.h
@@ -29,14 +29,23 @@ class EluBackwardGradInput : public Operator<EluBackwardGradInput> {
 
  protected:
   Tensor::Shape grad_output_shape_;
+
   Tensor::Strides grad_output_strides_;
+
   DataType grad_output_type_;
+
   Tensor::Shape self_or_result_shape_;
+
   Tensor::Strides self_or_result_strides_;
+
   DataType self_or_result_type_;
+
   Tensor::Shape grad_input_shape_;
+
   Tensor::Strides grad_input_strides_;
+
   DataType grad_input_type_;
+
   int device_index_{0};
 };
 
