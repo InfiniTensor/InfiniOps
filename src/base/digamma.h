47af5d1cdf316d9ef60e7aca90651f1c75fceddf
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:53:10 +0800

    feat: add `digamma` base

---
+++ src/base/digamma.h
@@ -0,0 +1,33 @@
+#ifndef INFINI_OPS_BASE_DIGAMMA_H_
+#define INFINI_OPS_BASE_DIGAMMA_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class Digamma : public Operator<Digamma> {
+ public:
+  Digamma(const Tensor self, Tensor out)
+      : self_shape_{self.shape()},
+        self_strides_{self.strides()},
+        self_type_{self.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor self, Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape self_shape_;
+  Tensor::Strides self_strides_;
+  DataType self_type_;
+  Tensor::Shape out_shape_;
+  Tensor::Strides out_strides_;
+  DataType out_type_;
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
