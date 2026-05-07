208cc232d02020a73769798979af1531770f3cb5
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:22 +0800

    chore: separate base class members

--- src/base/special_i0e.h
+++ src/base/special_i0e.h
@@ -20,11 +20,17 @@ class SpecialI0e : public Operator<SpecialI0e> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
