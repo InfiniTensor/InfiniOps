ddfafead4d6d702a4f5c7fdb730199950fd51af4
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:53:07 +0800

    feat: add `bmm` base

---
+++ src/base/bmm.h
@@ -0,0 +1,40 @@
+#ifndef INFINI_OPS_BASE_BMM_H_
+#define INFINI_OPS_BASE_BMM_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class Bmm : public Operator<Bmm> {
+ public:
+  Bmm(const Tensor self, const Tensor mat2, Tensor out)
+      : self_shape_{self.shape()},
+        self_strides_{self.strides()},
+        self_type_{self.dtype()},
+        mat2_shape_{mat2.shape()},
+        mat2_strides_{mat2.strides()},
+        mat2_type_{mat2.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor self, const Tensor mat2,
+                          Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape self_shape_;
+  Tensor::Strides self_strides_;
+  DataType self_type_;
+  Tensor::Shape mat2_shape_;
+  Tensor::Strides mat2_strides_;
+  DataType mat2_type_;
+  Tensor::Shape out_shape_;
+  Tensor::Strides out_strides_;
+  DataType out_type_;
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
