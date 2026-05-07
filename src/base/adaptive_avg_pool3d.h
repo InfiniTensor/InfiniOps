9e592671850351bd662ec62bb9b81fdffba9598a
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:36:46 +0800

    chore: separate base class members

--- src/base/adaptive_avg_pool3d.h
+++ src/base/adaptive_avg_pool3d.h
@@ -23,11 +23,17 @@ class AdaptiveAvgPool3d : public Operator<AdaptiveAvgPool3d> {
 
  protected:
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape out_shape_;
+
   Tensor::Strides out_strides_;
+
   DataType out_type_;
+
   int device_index_{0};
 };
 
