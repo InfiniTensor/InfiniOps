526c03505536bb74fd67618c4dd7fb95d26aa3b8
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:53:28 +0800

    feat: add `scatter_add` base

---
+++ src/base/scatter_add.h
@@ -0,0 +1,48 @@
+#ifndef INFINI_OPS_BASE_SCATTER_ADD_H_
+#define INFINI_OPS_BASE_SCATTER_ADD_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class ScatterAdd : public Operator<ScatterAdd> {
+ public:
+  ScatterAdd(const Tensor self, const int64_t dim, const Tensor index,
+             const Tensor src, Tensor out)
+      : self_shape_{self.shape()},
+        self_strides_{self.strides()},
+        self_type_{self.dtype()},
+        index_shape_{index.shape()},
+        index_strides_{index.strides()},
+        index_type_{index.dtype()},
+        src_shape_{src.shape()},
+        src_strides_{src.strides()},
+        src_type_{src.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor self, const int64_t dim,
+                          const Tensor index, const Tensor src,
+                          Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape self_shape_;
+  Tensor::Strides self_strides_;
+  DataType self_type_;
+  Tensor::Shape index_shape_;
+  Tensor::Strides index_strides_;
+  DataType index_type_;
+  Tensor::Shape src_shape_;
+  Tensor::Strides src_strides_;
+  DataType src_type_;
+  Tensor::Shape out_shape_;
+  Tensor::Strides out_strides_;
+  DataType out_type_;
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
