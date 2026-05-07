fd5508c3a0c68ee7ede38d9dc71863a22e39d908
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-07 17:37:31 +0800

    chore: separate base class members

--- src/base/glu_backward_grad_input.h
+++ src/base/glu_backward_grad_input.h
@@ -25,14 +25,23 @@ class GluBackwardGradInput : public Operator<GluBackwardGradInput> {
 
  protected:
   Tensor::Shape grad_output_shape_;
+
   Tensor::Strides grad_output_strides_;
+
   DataType grad_output_type_;
+
   Tensor::Shape self_shape_;
+
   Tensor::Strides self_strides_;
+
   DataType self_type_;
+
   Tensor::Shape grad_input_shape_;
+
   Tensor::Strides grad_input_strides_;
+
   DataType grad_input_type_;
+
   int device_index_{0};
 };
 
