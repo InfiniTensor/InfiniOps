9c2a120224e8cf9d418d47fbb0d1ab337fbbabd9
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-09 18:03:12 +0800

    feat: add `softshrink` base

---
+++ src/base/softshrink.h
@@ -0,0 +1,43 @@
+#ifndef INFINI_OPS_BASE_SOFTSHRINK_H_
+#define INFINI_OPS_BASE_SOFTSHRINK_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class Softshrink : public Operator<Softshrink> {
+ public:
+  Softshrink(const Tensor input, const double lambd, Tensor out)
+      : input_shape_{input.shape()},
+        input_strides_{input.strides()},
+        input_type_{input.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        lambd_{lambd},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor input, const double lambd,
+                          Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape input_shape_;
+
+  Tensor::Strides input_strides_;
+
+  DataType input_type_;
+
+  Tensor::Shape out_shape_;
+
+  Tensor::Strides out_strides_;
+
+  DataType out_type_;
+
+  double lambd_{};
+
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
