ceb397742509cb7c5cbee67ce49f6687cc46b367
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-05-09 18:10:21 +0800

    feat: add `upsample_trilinear3d` base

---
+++ src/base/upsample_trilinear3d.h
@@ -0,0 +1,49 @@
+#ifndef INFINI_OPS_BASE_UPSAMPLE_TRILINEAR3D_H_
+#define INFINI_OPS_BASE_UPSAMPLE_TRILINEAR3D_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class UpsampleTrilinear3d : public Operator<UpsampleTrilinear3d> {
+ public:
+  UpsampleTrilinear3d(const Tensor input,
+                      const std::vector<int64_t> output_size,
+                      const bool align_corners, Tensor out)
+      : input_shape_{input.shape()},
+        input_strides_{input.strides()},
+        input_type_{input.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        output_size_{output_size},
+        align_corners_{align_corners},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor input,
+                          const std::vector<int64_t> output_size,
+                          const bool align_corners, Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape input_shape_;
+
+  Tensor::Strides input_strides_;
+
+  DataType input_type_;
+
+  Tensor::Shape out_shape_;
+
+  Tensor::Strides out_strides_;
+
+  DataType out_type_;
+
+  std::vector<int64_t> output_size_{};
+
+  bool align_corners_{};
+
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
