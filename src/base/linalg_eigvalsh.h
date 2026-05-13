9a609c0d5a622a3b74ea849d5540a0c39e8a52d9
Jiacheng Huang <45955067+voltjia@users.noreply.github.com>
2026-05-13 13:55:45 +0800

    feat: add `linalg_eigvalsh` base (#267)

---
+++ src/base/linalg_eigvalsh.h
@@ -0,0 +1,43 @@
+#ifndef INFINI_OPS_BASE_LINALG_EIGVALSH_H_
+#define INFINI_OPS_BASE_LINALG_EIGVALSH_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class LinalgEigvalsh : public Operator<LinalgEigvalsh> {
+ public:
+  LinalgEigvalsh(const Tensor input, const std::string UPLO, Tensor out)
+      : input_shape_{input.shape()},
+        input_strides_{input.strides()},
+        input_type_{input.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        UPLO_{UPLO},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor input, const std::string UPLO,
+                          Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape input_shape_;
+
+  Tensor::Strides input_strides_;
+
+  DataType input_type_;
+
+  Tensor::Shape out_shape_;
+
+  Tensor::Strides out_strides_;
+
+  DataType out_type_;
+
+  std::string UPLO_{};
+
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
