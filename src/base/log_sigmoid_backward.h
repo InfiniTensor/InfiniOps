3e3b36372393f86067bd18aa1ac4bae4d419f960
Jiacheng Huang <45955067+voltjia@users.noreply.github.com>
2026-05-13 11:58:54 +0800

    feat: add `log_sigmoid_backward` base (#545)

---
+++ src/base/log_sigmoid_backward.h
@@ -0,0 +1,59 @@
+#ifndef INFINI_OPS_BASE_LOG_SIGMOID_BACKWARD_H_
+#define INFINI_OPS_BASE_LOG_SIGMOID_BACKWARD_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class LogSigmoidBackward : public Operator<LogSigmoidBackward> {
+ public:
+  LogSigmoidBackward(const Tensor grad_output, const Tensor input,
+                     const Tensor buffer, Tensor grad_input)
+      : grad_output_shape_{grad_output.shape()},
+        grad_output_strides_{grad_output.strides()},
+        grad_output_type_{grad_output.dtype()},
+        input_shape_{input.shape()},
+        input_strides_{input.strides()},
+        input_type_{input.dtype()},
+        buffer_shape_{buffer.shape()},
+        buffer_strides_{buffer.strides()},
+        buffer_type_{buffer.dtype()},
+        grad_input_shape_{grad_input.shape()},
+        grad_input_strides_{grad_input.strides()},
+        grad_input_type_{grad_input.dtype()},
+        device_index_{grad_input.device().index()} {}
+
+  virtual void operator()(const Tensor grad_output, const Tensor input,
+                          const Tensor buffer, Tensor grad_input) const = 0;
+
+ protected:
+  Tensor::Shape grad_output_shape_;
+
+  Tensor::Strides grad_output_strides_;
+
+  DataType grad_output_type_;
+
+  Tensor::Shape input_shape_;
+
+  Tensor::Strides input_strides_;
+
+  DataType input_type_;
+
+  Tensor::Shape buffer_shape_;
+
+  Tensor::Strides buffer_strides_;
+
+  DataType buffer_type_;
+
+  Tensor::Shape grad_input_shape_;
+
+  Tensor::Strides grad_input_strides_;
+
+  DataType grad_input_type_;
+
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
