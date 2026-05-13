6f88c75d50324fb9b9ec41eb5ca206252c50627c
Jiacheng Huang <45955067+voltjia@users.noreply.github.com>
2026-05-13 13:49:35 +0800

    feat: add `cummax` base (#154)

---
+++ src/base/cummax.h
@@ -0,0 +1,52 @@
+#ifndef INFINI_OPS_BASE_CUMMAX_H_
+#define INFINI_OPS_BASE_CUMMAX_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class Cummax : public Operator<Cummax> {
+ public:
+  Cummax(const Tensor input, const int64_t dim, Tensor values, Tensor indices)
+      : input_shape_{input.shape()},
+        input_strides_{input.strides()},
+        input_type_{input.dtype()},
+        values_shape_{values.shape()},
+        values_strides_{values.strides()},
+        values_type_{values.dtype()},
+        indices_shape_{indices.shape()},
+        indices_strides_{indices.strides()},
+        indices_type_{indices.dtype()},
+        dim_{dim},
+        device_index_{values.device().index()} {}
+
+  virtual void operator()(const Tensor input, const int64_t dim, Tensor values,
+                          Tensor indices) const = 0;
+
+ protected:
+  Tensor::Shape input_shape_;
+
+  Tensor::Strides input_strides_;
+
+  DataType input_type_;
+
+  Tensor::Shape values_shape_;
+
+  Tensor::Strides values_strides_;
+
+  DataType values_type_;
+
+  Tensor::Shape indices_shape_;
+
+  Tensor::Strides indices_strides_;
+
+  DataType indices_type_;
+
+  int64_t dim_{};
+
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
