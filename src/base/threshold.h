d567d275d6297ae57d617967f0ab1141e0d27faa
Jiacheng Huang <45955067+voltjia@users.noreply.github.com>
2026-05-13 12:03:00 +0800

    feat: add `threshold` base (#507)

---
+++ src/base/threshold.h
@@ -0,0 +1,47 @@
+#ifndef INFINI_OPS_BASE_THRESHOLD_H_
+#define INFINI_OPS_BASE_THRESHOLD_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class Threshold : public Operator<Threshold> {
+ public:
+  Threshold(const Tensor input, const double threshold, const double value,
+            Tensor out)
+      : input_shape_{input.shape()},
+        input_strides_{input.strides()},
+        input_type_{input.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        threshold_{threshold},
+        value_{value},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor input, const double threshold,
+                          const double value, Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape input_shape_;
+
+  Tensor::Strides input_strides_;
+
+  DataType input_type_;
+
+  Tensor::Shape out_shape_;
+
+  Tensor::Strides out_strides_;
+
+  DataType out_type_;
+
+  double threshold_{};
+
+  double value_{};
+
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
