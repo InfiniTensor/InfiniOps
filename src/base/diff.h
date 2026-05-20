97074f52643ce6497d20ff5c5938e4b72331ae82
Jiacheng Huang <45955067+voltjia@users.noreply.github.com>
2026-05-20 12:49:13 +0800

    feat: add `diff` base (#160)
    * feat: add `diff` base
    
    * fix: add trailing newline to base header

---
+++ src/base/diff.h
@@ -0,0 +1,46 @@
+#ifndef INFINI_OPS_BASE_DIFF_H_
+#define INFINI_OPS_BASE_DIFF_H_
+
+#include "operator.h"
+
+namespace infini::ops {
+
+class Diff : public Operator<Diff> {
+ public:
+  Diff(const Tensor input, const int64_t n, const int64_t dim, Tensor out)
+      : input_shape_{input.shape()},
+        input_strides_{input.strides()},
+        input_type_{input.dtype()},
+        out_shape_{out.shape()},
+        out_strides_{out.strides()},
+        out_type_{out.dtype()},
+        n_{n},
+        dim_{dim},
+        device_index_{out.device().index()} {}
+
+  virtual void operator()(const Tensor input, const int64_t n,
+                          const int64_t dim, Tensor out) const = 0;
+
+ protected:
+  Tensor::Shape input_shape_;
+
+  Tensor::Strides input_strides_;
+
+  DataType input_type_;
+
+  Tensor::Shape out_shape_;
+
+  Tensor::Strides out_strides_;
+
+  DataType out_type_;
+
+  int64_t n_{};
+
+  int64_t dim_{};
+
+  int device_index_{0};
+};
+
+}  // namespace infini::ops
+
+#endif
