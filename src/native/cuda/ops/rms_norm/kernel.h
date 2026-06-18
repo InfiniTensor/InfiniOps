5cb348719380fc6c05beffec634e3170129ac353
thatPepe <wooway777@gmail.com>
2026-06-18 11:17:15 +0800

    fix: support combined dtype in rms norm (#731)

--- src/native/cuda/ops/rms_norm/kernel.h
+++ src/native/cuda/ops/rms_norm/kernel.h
@@ -32,23 +32,28 @@ class CudaRmsNorm : public RmsNorm {
 
     uint32_t num_blocks = static_cast<uint32_t>(batch_size_ * nhead_);
 
-    assert(out.dtype() == input.dtype() && out.dtype() == weight.dtype());
+    assert(out.dtype() == input.dtype());
 
     int block_size = RuntimeUtils<Backend::kDeviceType>::GetOptimalBlockSize();
 
     DispatchFunc<ConcatType<List<DataType::kFloat32>, ReducedFloatTypes>,
+                 ConcatType<List<DataType::kFloat32>, ReducedFloatTypes>,
                  AllCudaBlockSizes>(
-        {static_cast<int64_t>(out.dtype()), block_size},
+        {static_cast<int64_t>(out.dtype()),
+         static_cast<int64_t>(weight.dtype()), block_size},
         [&](auto list_tag) {
           using T = TypeMapType<Backend::kDeviceType, ListGet<0>(list_tag)>;
-          constexpr int kBlockSize = ListGet<1>(list_tag);
+          using TWeight =
+              TypeMapType<Backend::kDeviceType, ListGet<1>(list_tag)>;
+          constexpr int kBlockSize = ListGet<2>(list_tag);
 
-          RmsNormKernel<kBlockSize, Backend::kDeviceType, float, T, T>
+          RmsNormKernel<kBlockSize, Backend::kDeviceType, float, T, TWeight>
               <<<num_blocks, kBlockSize, 0, cuda_stream>>>(
                   reinterpret_cast<T*>(out.data()), stride_out_batch,
                   stride_out_nhead, reinterpret_cast<const T*>(input.data()),
                   stride_input_batch, stride_input_nhead,
-                  reinterpret_cast<const T*>(weight.data()), nhead_, dim_, eps);
+                  reinterpret_cast<const TWeight*>(weight.data()), nhead_, dim_,
+                  eps);
         },
         "CudaRmsNorm::operator()");
   }
