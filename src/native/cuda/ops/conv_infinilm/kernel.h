baf031b7cc76a78fd9c637b5a178d5d0edb6c6ca
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-08-07 10:43:04 +0800

    refactor(cuda): adapt conv infinilm to convolution

--- src/native/cuda/ops/conv_infinilm/kernel.h
+++ src/native/cuda/ops/conv_infinilm/kernel.h
@@ -1,19 +1,12 @@
 #ifndef INFINI_OPS_CUDA_CONV_INFINILM_KERNEL_H_
 #define INFINI_OPS_CUDA_CONV_INFINILM_KERNEL_H_
 
-#include <algorithm>
-#include <cstddef>
-#include <cstring>
+#include <cstdint>
 #include <optional>
 #include <vector>
 
 #include "base/conv_infinilm.h"
-#include "common/generic_utils.h"
-#include "data_type.h"
-#include "dispatcher.h"
-#include "native/cuda/kernel_commons.cuh"
-#include "native/cuda/ops/convolution/kernel.cuh"
-#include "native/cuda/runtime_utils.h"
+#include "native/cuda/ops/convolution/kernel.h"
 
 namespace infini::ops {
 
@@ -27,76 +20,10 @@ class CudaConvInfinilm : public ConvInfinilm {
                    const std::vector<int64_t> dilation, const int64_t groups,
                    Tensor out)
       : ConvInfinilm{input,  weight,   bias,   padding,
-                     stride, dilation, groups, out} {
-    size_t input_shape_size = input_shape_.size() * sizeof(*d_input_shape_);
-    size_t weight_shape_size = weight_shape_.size() * sizeof(*d_weight_shape_);
-    size_t out_shape_size = out_shape_.size() * sizeof(*d_out_shape_);
-    size_t input_strides_size =
-        input_strides_.size() * sizeof(*d_input_strides_);
-    size_t weight_strides_size =
-        weight_strides_.size() * sizeof(*d_weight_strides_);
-    size_t out_strides_size = out_strides_.size() * sizeof(*d_out_strides_);
-    size_t bias_strides_size = sizeof(*d_bias_strides_);
-    size_t attrs_size = spatial_ndim_ * sizeof(*d_padding_);
-    const size_t metadata_size = input_shape_size + weight_shape_size +
-                                 out_shape_size + input_strides_size +
-                                 weight_strides_size + out_strides_size +
-                                 bias_strides_size + 3 * attrs_size;
-    std::vector<std::byte> metadata(metadata_size);
-
-    Backend::Malloc((void**)&d_metadata_, metadata_size);
-
-    size_t offset = 0;
-    d_input_shape_ = reinterpret_cast<Tensor::Size*>(d_metadata_ + offset);
-    std::memcpy(metadata.data() + offset, input_shape_.data(),
-                input_shape_size);
-    offset += input_shape_size;
-
-    d_weight_shape_ = reinterpret_cast<Tensor::Size*>(d_metadata_ + offset);
-    std::memcpy(metadata.data() + offset, weight_shape_.data(),
-                weight_shape_size);
-    offset += weight_shape_size;
-
-    d_out_shape_ = reinterpret_cast<Tensor::Size*>(d_metadata_ + offset);
-    std::memcpy(metadata.data() + offset, out_shape_.data(), out_shape_size);
-    offset += out_shape_size;
-
-    d_input_strides_ = reinterpret_cast<Tensor::Stride*>(d_metadata_ + offset);
-    std::memcpy(metadata.data() + offset, input_strides_.data(),
-                input_strides_size);
-    offset += input_strides_size;
-
-    d_weight_strides_ = reinterpret_cast<Tensor::Stride*>(d_metadata_ + offset);
-    std::memcpy(metadata.data() + offset, weight_strides_.data(),
-                weight_strides_size);
-    offset += weight_strides_size;
-
-    d_out_strides_ = reinterpret_cast<Tensor::Stride*>(d_metadata_ + offset);
-    std::memcpy(metadata.data() + offset, out_strides_.data(),
-                out_strides_size);
-    offset += out_strides_size;
-
-    d_bias_strides_ = reinterpret_cast<Tensor::Stride*>(d_metadata_ + offset);
-    Tensor::Stride bias_stride = has_bias_ ? bias_strides_[0] : 0;
-    std::memcpy(metadata.data() + offset, &bias_stride, bias_strides_size);
-    offset += bias_strides_size;
-
-    d_padding_ = reinterpret_cast<int64_t*>(d_metadata_ + offset);
-    std::memcpy(metadata.data() + offset, padding_.data(), attrs_size);
-    offset += attrs_size;
-
-    d_stride_ = reinterpret_cast<int64_t*>(d_metadata_ + offset);
-    std::memcpy(metadata.data() + offset, stride_.data(), attrs_size);
-    offset += attrs_size;
-
-    d_dilation_ = reinterpret_cast<int64_t*>(d_metadata_ + offset);
-    std::memcpy(metadata.data() + offset, dilation_.data(), attrs_size);
-
-    Backend::Memcpy(d_metadata_, metadata.data(), metadata_size,
-                    Backend::kMemcpyHostToDevice);
-  }
-
-  ~CudaConvInfinilm() { Backend::Free(d_metadata_); }
+                     stride, dilation, groups, out},
+        output_padding_(input.ndim() - 2, 0),
+        convolution_{input, weight,          bias,   stride, padding, dilation,
+                     false, output_padding_, groups, out} {}
 
   void operator()(const Tensor input, const Tensor weight,
                   std::optional<Tensor> bias,
@@ -104,63 +31,15 @@ class CudaConvInfinilm : public ConvInfinilm {
                   const std::vector<int64_t> stride,
                   const std::vector<int64_t> dilation, const int64_t groups,
                   Tensor out) const override {
-    (void)padding;
-    (void)stride;
-    (void)dilation;
-    (void)groups;
-    auto cuda_stream =
-        static_cast<typename Backend::Stream>(stream_ ? stream_ : 0);
-    int block_size = std::min(
-        RuntimeUtils<Backend::kDeviceType>::GetOptimalBlockSize(), 1024);
-    dim3 block(std::min(static_cast<Tensor::Size>(block_size), output_size_));
-    dim3 grid(utils::CeilDiv(output_size_, block.x));
-
-    DispatchFunc<ConcatType<List<DataType::kFloat32>, ReducedFloatTypes>,
-                 List<128, 256, 512, 1024>>(
-        {static_cast<int64_t>(out_type_), block_size},
-        [&](auto list_tag) {
-          using T = TypeMapType<Backend::kDeviceType, ListGet<0>(list_tag)>;
-          constexpr int kBlockSize = ListGet<1>(list_tag);
-          const T* bias_ptr = bias.has_value()
-                                  ? reinterpret_cast<const T*>(bias->data())
-                                  : nullptr;
-
-          ConvKernel<Backend::kDeviceType, T, kBlockSize>
-              <<<grid, block, 0, cuda_stream>>>(
-                  reinterpret_cast<T*>(out.data()),
-                  reinterpret_cast<const T*>(input.data()),
-                  reinterpret_cast<const T*>(weight.data()), bias_ptr,
-                  d_input_shape_, d_weight_shape_, d_out_shape_,
-                  d_input_strides_, d_weight_strides_, d_out_strides_,
-                  d_bias_strides_, d_padding_, d_stride_, d_dilation_,
-                  output_size_, spatial_ndim_, kernel_size_, groups_,
-                  has_bias_);
-        },
-        "CudaConvInfinilm::operator()");
+    convolution_.set_stream(stream_);
+    convolution_(input, weight, bias, stride, padding, dilation, false,
+                 output_padding_, groups, out);
   }
 
  private:
-  std::byte* d_metadata_{nullptr};
+  std::vector<int64_t> output_padding_;
 
-  Tensor::Size* d_input_shape_{nullptr};
-
-  Tensor::Size* d_weight_shape_{nullptr};
-
-  Tensor::Size* d_out_shape_{nullptr};
-
-  Tensor::Stride* d_input_strides_{nullptr};
-
-  Tensor::Stride* d_weight_strides_{nullptr};
-
-  Tensor::Stride* d_out_strides_{nullptr};
-
-  Tensor::Stride* d_bias_strides_{nullptr};
-
-  int64_t* d_padding_{nullptr};
-
-  int64_t* d_stride_{nullptr};
-
-  int64_t* d_dilation_{nullptr};
+  mutable CudaConv<Backend, Convolution> convolution_;
 };
 
 }  // namespace infini::ops
